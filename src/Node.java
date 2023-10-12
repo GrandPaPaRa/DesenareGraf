422aea98e13ec63e65cc08ee5c3740d34c18120e
Toma Gheorghe Claudiu <gheorghe.toma@student.unitbv.ro>
2023-10-12 19:07:37 +0300

    Initial build with 2 and 3 task

---
+++ src/Node.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class Node
+{
+	private int coordX;
+	private int coordY;
+	private int number;
+	
+	public Node(int coordX, int coordY, int number)
+	{
+		this.coordX = coordX;
+		this.coordY = coordY;
+		this.number = number;
+	}
+	
+	public int getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+	public int getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void drawNode(Graphics g, int node_diam)
+	{
+		g.setColor(Color.RED);
+		g.setFont(new Font("TimesRoman", Font.BOLD, 15));
+        g.fillOval(coordX - node_diam / 2, coordY - node_diam / 2, node_diam , node_diam);
+        g.setColor(Color.WHITE);
+        g.drawOval(coordX - node_diam / 2, coordY - node_diam / 2, node_diam, node_diam);
+        if(number < 10)
+        	g.drawString(((Integer)number).toString(), coordX+12 - node_diam / 2, coordY+20 - node_diam / 2);
+        else
+        	g.drawString(((Integer)number).toString(), coordX+8 - node_diam / 2, coordY+20 - node_diam / 2);
+	}
+}
